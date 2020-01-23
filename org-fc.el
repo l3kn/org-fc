4c8c77c88d741161fdc6ec09bee939bdf9a1ea21
Leon Rische <leon.rische@me.com>
2020-01-23 15:32:55 +0100

    Add GPL Licence

--- org-fc.el
+++ org-fc.el
@@ -1,3 +1,25 @@
+;;; org-fc.el --- Spaced Repetition System for Emacs org-mode -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Leon Rische
+
+;; Author: Leon Rische <emacs@leonrische.me>
+;; Url: https://www.leonrische.me/pages/org_flashcards.html
+;; Package-requires: ((emacs "26.3") (tablist "0.15.0"))
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (require 'hydra)
 
 (require 'cl)
