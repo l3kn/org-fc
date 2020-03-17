cca547f75483f117262d8f7c005fd67368c706c8
Leon Rische <leon.rische@me.com>
2020-03-17 22:25:31 +0100

    Remove text-input cards

--- org-fc.el
+++ org-fc.el
@@ -641,34 +641,6 @@ Processes all holes in the card text."
  'org-fc-type-cloze-flip
  'org-fc-type-cloze-update)
 
-;;;; Text-Input
-
-(defun org-fc-type-text-input-init ()
-  (interactive)
-  (org-fc--init-card "text-input")
-  (org-fc-review-data-update '("front")))
-
-(defun org-fc-type-text-input-review (_position)
-  (org-show-subtree)
-  (let ((answer (org-entry-get (point) "ANSWER"))
-        (user-answer (read-string "Answer: ")))
-    (goto-char (point-max))
-    ;; Overlays need to be of at least size 1 to be visible
-    (let ((ovl (make-overlay (- (point) 1) (point))))
-      (overlay-put ovl 'category 'org-fc-additional-text-overlay)
-      (overlay-put ovl 'priority 9999)
-      (overlay-put ovl 'face 'default)
-      (overlay-put ovl 'display
-                   (concat "\n\n\nExpected: " answer
-                           "\nGot:      " user-answer)))))
-
-;; TODO: Implement real handler
-(org-fc-register-type
- 'text-input
- 'org-fc-type-normal-setup
- 'org-fc-type-normal-flip
- 'org-fc-noop)
-
 ;;; Working with Overlays / Hiding Text
 ;;;; Finding Locations in the Buffer
 
