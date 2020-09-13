87115259064ffcc69c9a76afa6557a728d199f56
Leon Rische <leon.rische@me.com>
2020-09-13 12:39:40 +0200

    Replace use of `org-fc-content-position`

--- org-fc.el
+++ org-fc.el
@@ -814,7 +814,7 @@ CURRENT-INDEX is the index of the current position in the list of all holes."
   "Hide holes of a card of TYPE in relation to POSITION."
   (org-fc-with-point-at-entry
    (let* ((type (intern (org-entry-get (point) org-fc-type-cloze-type-property)))
-          (end (cdr (org-fc-content-position)))
+          (end (org-element-property :contents-end (org-element-at-point)))
           (holes-index (org-fc-type-cloze--parse-holes position end))
           (holes (car holes-index))
           (current-index (cdr holes-index)))
@@ -889,7 +889,7 @@ Processes all holes in the card text."
 
 (defun org-fc-type-cloze-update ()
   "Update the review data & deletions of the current heading."
-  (let* ((end (cdr (org-fc-content-position)))
+  (let* ((end (org-element-property :contents-end (org-element-at-point)))
          (hole-id (1+ (org-fc-type-cloze-max-hole-id)))
          ids)
     (save-excursion
