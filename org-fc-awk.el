ff18f6d0a031ba8c723b45563261f1329b14d03b
Leon Rische <leon.rische@me.com>
2023-12-31 15:27:28 +0100

    Bump version to 0.2.0

--- org-fc-awk.el
+++ org-fc-awk.el
@@ -1,11 +1,11 @@
 ;;; org-awk.el --- AWK based flashcard indexing -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2020-2021  Leon Rische
+;; Copyright (C) 2020-2024  Leon Rische
 
 ;; Author: Leon Rische <emacs@leonrische.me>
 ;; Url: https://www.leonrische.me/pages/org_flashcards.html
 ;; Package-requires: ((emacs "26.3") (org "9.3"))
-;; Version: 0.1.0
+;; Version: 0.2.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
