cface2897ac0971aa0a53ee7afe4206c662b7dde
Leon Rische <leon.rische@me.com>
2024-01-03 13:37:37 +0100

    Add changelog and bump to version v0.3.0

--- org-fc.el
+++ org-fc.el
@@ -5,7 +5,7 @@
 ;; Author: Leon Rische <emacs@leonrische.me>
 ;; Url: https://www.leonrische.me/pages/org_flashcards.html
 ;; Package-requires: ((emacs "26.3") (org "9.3"))
-;; Version: 0.2.0
+;; Version: 0.3.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
