95780b8a99a9eea9cf6d3bb3c703fbd70b3bf695
Leon Rische <leon.rische@me.com>
2024-01-03 13:33:01 +0100

    Make sections collapsible and add documentation

--- org-fc-dashboard.el
+++ org-fc-dashboard.el
@@ -27,6 +27,8 @@
 ;;
 ;;; Code:
 
+(require 'cl-lib)
+(require 'eieio)
 (require 'svg)
 
 (require 'org-fc-core)
@@ -59,8 +61,14 @@ environment without svg support."
 
 ;;; Variables
 
-(defvar org-fc-dashboard-context org-fc-context-all
+(defvar-local org-fc-dashboard-context org-fc-context-all
   "Context of the current dashboard view.")
+
+(defvar-local org-fc-dashboard-cards '()
+  "Cards of the current dashboard view/context.")
+
+(defvar-local org-fc-dashboard-section--current 0
+  "Index of the current dashboard section.")
 
 ;;; Helper Functions
 
@@ -71,7 +79,146 @@ environment without svg support."
       (push (cons key (gethash key ht)) res))
     res))
 
+;;; Sections and Folding
 
+(defclass org-fc-dashboard-section ()
+  ((title
+    :initarg :title)
+   (inserter
+    :initarg :inserter)
+   (start-visible
+    :initarg :start-visible
+    :initform nil)
+   (visible
+    :initarg :visible
+    :initform nil)
+   (expanded
+    :initarg :expanded
+    :initform nil)
+   (heading-bounds
+    :initarg :heading-bounds
+    :initform nil)
+   (content-bounds
+    :initarg :content-bounds
+    :initform nil)))
+
+(cl-defmethod org-fc-dashboard-section-offset-bounds ((section org-fc-dashboard-section) delta)
+  "Offset all bounds of a SECTION by DELTA characters."
+  (with-slots (heading-bounds content-bounds) section
+    (oset
+     section heading-bounds
+     (cons (+ (car heading-bounds) delta)
+           (+ (cdr heading-bounds) delta)))
+    (oset
+     section content-bounds
+     (cons (+ (car content-bounds) delta)
+           (+ (cdr content-bounds) delta)))))
+
+(defun org-fc-dashboard-section-goto (n &optional is-first)
+  (let ((inhibit-read-only t))
+    ;; If there was a previous section, change the cursor-marker
+    (unless is-first
+      (let ((section-old
+	     (nth
+	      org-fc-dashboard-section--current
+	      org-fc-dashboard-sections)))
+	(goto-char (car (oref section-old heading-bounds)))
+	(delete-char 1)
+	(if (oref section-old visible)
+	    (insert (propertize "-" 'face 'org-level-1))
+	  (insert (propertize "+" 'face 'org-level-1)))))
+
+    (let ((section-new (nth n org-fc-dashboard-sections)))
+      (goto-char (car (oref section-new heading-bounds)))
+      (delete-char 1)
+      (insert (propertize "o" 'face 'org-level-1)))
+    (setq org-fc-dashboard-section--current n)))
+
+(cl-defmethod org-fc-dashboard-section-close ((section org-fc-dashboard-section))
+  "Close a dashboard SECTION."
+  (when (oref section visible)
+    (let ((inhibit-read-only t))
+      (goto-char (car (oref section heading-bounds)))
+      (unless (= (following-char) ?o)
+	(delete-char 1)
+	(insert (propertize "+" 'face 'org-level-1)))
+      (org-fc-hide-region
+       (car (oref section content-bounds))
+       (cdr (oref section content-bounds)))
+      (oset section visible nil)
+      ;; In case point is inside the section,
+      ;; jump to the “headline” when collapsing
+      (goto-char (car (oref section heading-bounds))))))
+
+(cl-defmethod org-fc-dashboard-section-open ((section org-fc-dashboard-section))
+  "Open a dashboard SECTION."
+  (unless (oref section visible)
+    (let ((inhibit-read-only t))
+      (save-excursion
+	(goto-char (car (oref section heading-bounds)))
+	(unless (= (following-char) ?o)
+	  (delete-char 1)
+	  (insert (propertize "-" 'face 'org-level-1))))
+      (if (oref section expanded)
+	  (progn
+	    (remove-overlays
+	     (car (oref section content-bounds))
+	     (cdr (oref section content-bounds))
+	     'category 'org-fc)
+	    (oset section visible t))
+	(save-excursion
+	  (goto-char (car (oref section content-bounds)))
+	  (let ((cur (point)))
+	    (funcall (oref section inserter))
+	    (oset section content-bounds (cons cur (point)))
+	    (oset section expanded t)
+	    (oset section visible t)
+
+	    (let ((delta (- (point) cur)))
+	      (dolist (next-section org-fc-dashboard-sections)
+		(when (and (not (equal section next-section))
+			   (<= cur (car (oref next-section heading-bounds))))
+		  (org-fc-dashboard-section-offset-bounds next-section delta))))))))))
+
+(defun org-fc-dashboard-section-toggle ()
+  "Toggle the current dashboard section."
+  (interactive)
+  (let ((section (nth org-fc-dashboard-section--current org-fc-dashboard-sections)))
+    (if (oref section visible)
+	(org-fc-dashboard-section-close section)
+      (org-fc-dashboard-section-open section))))
+
+(defun org-fc-dashboard-section-previous ()
+  (interactive)
+  (if (= 0 org-fc-dashboard-section--current)
+      (org-fc-dashboard-section-goto (- (length org-fc-dashboard-sections) 1))
+    (org-fc-dashboard-section-goto (- org-fc-dashboard-section--current 1))))
+
+(defun org-fc-dashboard-section-next ()
+  (interactive)
+  (org-fc-dashboard-section-goto (mod (+ org-fc-dashboard-section--current 1) (length org-fc-dashboard-sections))))
+
+;; (defvar org-fc-dashboard-sections
+(setq org-fc-dashboard-sections
+  (list
+   (org-fc-dashboard-section
+    :title "Hotkeys"
+    :start-visible t
+    :inserter #'org-fc-dashboard-insert-hotkeys)
+   (org-fc-dashboard-section
+    :title "Overview"
+    :start-visible t
+    :inserter #'org-fc-dashboard-insert-overview)
+   (org-fc-dashboard-section
+    :title "Cards by Type"
+    :inserter #'org-fc-dashboard-insert-cards-by-type)
+   (org-fc-dashboard-section
+    :title "SM2 Parameters"
+    :inserter #'org-fc-dashboard-insert-sm2-stats)
+   (org-fc-dashboard-section
+    :title "Review History (All Cards)"
+    :start-visible t
+    :inserter #'org-fc-dashboard-insert-review-stats)))
 
 ;;; Bar Chart Generation
 
@@ -122,8 +269,13 @@ environment without svg support."
 
 ;;; Section Definitions
 
-(defun org-fc-dashboard-insert-overview (cards)
-  (let* ((card-count (length cards)) (suspended 0) (pos-count 0)
+(defun org-fc-dashboard-insert-hotkeys ()
+  (insert "\n")
+  (insert "  [r] review [q] quit\n")
+  (insert "  [n] next section [p] previous section [Tab] open/close section\n\n"))
+
+(defun org-fc-dashboard-insert-overview ()
+  (let* ((card-count (length org-fc-dashboard-cards)) (suspended 0) (pos-count 0)
          (created (list :day 0 :week 0 :month 0))
          (due (list :now 0 :day 0 :week 0 :month 0))
          (now (current-time))
@@ -133,7 +285,7 @@ environment without svg support."
          (plus-day (time-add now (* 24 60 60)))
          (plus-week (time-add now (* 7 24 60 60)))
          (plus-month (time-add now (* 30 24 60 60))))
-    (dolist (card cards)
+    (dolist (card org-fc-dashboard-cards)
       (if (plist-get card :suspended)
           (cl-incf suspended 1)
         (let ((card-created (plist-get card :created)))
@@ -158,9 +310,7 @@ environment without svg support."
               (if (time-less-p pos-due plus-month)
                   (cl-incf (cl-getf due :month) 1)))))))
 
-    (insert
-     (propertize "Overview\n\n" 'face 'org-level-1))
-
+    (insert "\n")
     (insert (format
 	     "  %d cards, %d suspended\n"
 	     card-count suspended))
@@ -174,28 +324,29 @@ environment without svg support."
     (insert (format "  %d positions\n" pos-count))
 
     (when (plusp pos-count)
-      (insert (format "  due: %d (now) %d (day) %d (week) %d (month)\n\n"
+      (insert (format "  due: %d (now) %d (day) %d (week) %d (month)\n"
 		      (plist-get due :now)
 		      (plist-get due :day)
 		      (plist-get due :week)
-		      (plist-get due :month))))))
+		      (plist-get due :month))))
+    (insert "\n")))
 
-(defun org-fc-dashboard-insert-cards-by-type (cards)
+(defun org-fc-dashboard-insert-cards-by-type ()
+  (insert "\n")
   (let* ((by-type (make-hash-table)))
-    (dolist (card cards)
+    (dolist (card org-fc-dashboard-cards)
       (unless (plist-get card :suspended)
 	(cl-incf (gethash (plist-get card :type) by-type 0) 1)))
 
-    (insert
-     (propertize "Cards by Type\n\n" 'face 'org-level-1))
-    (dolist (pair (org-fc-dashboard--hashtable-to-alist by-type))
-      (insert (format "  %6d %s\n" (cdr pair) (car pair))))
+    (if (plusp (hash-table-count by-type))
+	(dolist (pair (org-fc-dashboard--hashtable-to-alist by-type))
+	  (insert (format "  %6d %s\n" (cdr pair) (car pair))))
+      (insert "  No cards yet\n"))
     (insert "\n")))
 
-(defun org-fc-dashboard-insert-review-stats (_cards)
+(defun org-fc-dashboard-insert-review-stats ()
   (let ((reviews-stats (org-fc-awk-stats-reviews)))
-    (insert
-     (propertize "Review Statistics (All Cards)\n\n" 'face 'org-level-1))
+    (insert "\n")
     (if reviews-stats
 	(progn
 	  (dolist (scope '((:day . "day")
@@ -213,10 +364,10 @@ environment without svg support."
 	  (insert "\n"))
       (insert "  No reviews yet\n\n"))))
 
-(defun org-fc-dashboard-insert-sm2-stats (cards)
+(defun org-fc-dashboard-insert-sm2-stats ()
   (let* ((pos-count 0)
          (avg-ease 0.0) (avg-box 0.0) (avg-interval 0.0))
-    (dolist (card cards)
+    (dolist (card org-fc-dashboard-cards)
       (unless (plist-get card :suspended)
         (dolist (pos (plist-get card :positions))
           (cl-incf pos-count 1)
@@ -224,13 +375,13 @@ environment without svg support."
           (cl-incf avg-box (plist-get pos :box))
           (cl-incf avg-interval (plist-get pos :interval)))))
 
-    (insert
-     (propertize "SM2 Statistics\n\n" 'face 'org-level-1))
-
-    (when (plusp pos-count)
-      (insert (format "  %6.2f avg. ease\n" (/ avg-ease pos-count)))
-      (insert (format "  %6.2f avg. box\n" (/ avg-box pos-count)))
-      (insert (format "  %6.2f avg. interval (days)\n\n" (/ avg-interval pos-count))))))
+    (insert "\n")
+    (if (plusp pos-count)
+	(progn
+	  (insert (format "  %6.2f avg. ease\n" (/ avg-ease pos-count)))
+	  (insert (format "  %6.2f avg. box\n" (/ avg-box pos-count)))
+	  (insert (format "  %6.2f avg. interval (days)\n\n" (/ avg-interval pos-count))))
+      (insert "  No positions yet\n\n"))))
 
 ;;; Main View
 
@@ -241,17 +392,30 @@ environment without svg support."
          (inhibit-read-only t)
          (index (org-fc-index context)))
     (with-current-buffer buf
+      (setq org-fc-dashboard-context context)
+      (setq org-fc-dashboard-cards index)
+
       (erase-buffer)
 
-      (org-fc-dashboard-insert-overview index)
-      (org-fc-dashboard-insert-cards-by-type index)
-      (org-fc-dashboard-insert-sm2-stats index)
-      (org-fc-dashboard-insert-review-stats index)
+      (dolist (section org-fc-dashboard-sections)
+        (oset section expanded nil)
+        (oset section visible nil)
+        (let ((heading-beginning (point))
+              (heading-end nil)
+              (content-end nil))
+          (insert
+           (propertize
+            (format "+ %s\n" (oref section title))
+            'face 'org-level-1))
+          (setq heading-end (point))
+          (oset section heading-bounds (cons heading-beginning heading-end))
+          (setq content-end (point))
+          (oset section content-bounds (cons heading-end content-end))
+	  (when (oref section start-visible)
+	    (org-fc-dashboard-section-open section)
+	    (goto-char (cdr (oref section content-bounds))))))
 
-      (insert
-       (propertize "[r] Review\n" 'face 'org-level-1))
-      (insert
-       (propertize "[q] Quit\n" 'face 'org-level-1)))))
+      (org-fc-dashboard-section-goto 0 'is-first))))
 
 ;;; Dashboard Mode
 
@@ -269,22 +433,27 @@ environment without svg support."
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "r") 'org-fc-dashboard-review)
     (define-key map (kbd "q") 'quit-window)
+
+    (define-key map (kbd "n") 'org-fc-dashboard-section-next)
+    (define-key map (kbd "p") 'org-fc-dashboard-section-previous)
+    (define-key map (kbd "TAB") 'org-fc-dashboard-section-toggle)
+
     map))
 
-(define-derived-mode org-fc-dashboard-mode special-mode "org-fc main"
+(define-derived-mode org-fc-dashboard-mode special-mode "org-fc dashboard"
   "Major mode providing an overview of the flashcard system"
-  (set (make-local-variable 'revert-buffer-function) #'org-fc-dashboard-revert)
+  (setq-local revert-buffer-function #'org-fc-dashboard-revert)
   (setq-local cursor-type nil))
 
 ;;;###autoload
 (defun org-fc-dashboard (context)
   "Open a buffer showing the dashboard view for CONTEXT."
   (interactive (list (org-fc-select-context)))
-  (setq org-fc-dashboard-context context)
   (org-fc-dashboard-view context)
   (switch-to-buffer org-fc-dashboard-buffer-name)
-  (goto-char (point-min))
   (org-fc-dashboard-mode))
+
+;; TODO: Add documentation for new dashboard
 
 ;;; Footer
 
