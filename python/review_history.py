cddbf5f6b19c8c25e57ec507dcb588473c9c2250
Leon Rische <git@leonrische.me>
2025-08-21 17:14:53 +0200

    Add data models and TSV history parser

---
+++ python/review_history.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import csv
+import logging
+
+from models import Indentifier, Review
+
+
+class TSVReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_reviews(self) -> list[Review]:
+        reviews = []
+        with open(self.file_path, "r") as file:
+            reader = csv.reader(file, delimiter="\t")
+            for row in reader:
+                assert len(row) == 10, "Invalid review history entry"
+                datetime_str, _path, card_id, pos_name, _ease, _box, _interval, rating, duration, _algo = row
+
+                try:
+                    dt = datetime.fromisoformat(datetime_str)
+                except ValueError:
+                    logging.error(f"Invalid datetime format: {datetime_str}, skipping entry.")
+                    continue
+
+                if duration == "":
+                    duration = None
+                else:
+                    try:
+                        duration = float(duration)
+                    except ValueError:
+                        logging.error(f"Invalid duration format: {duration}, skipping entry.")
+                        continue
+
+                position = Indentifier(card_id=card_id, name=pos_name)
+                review = Review(identifier=position, rating=rating, datetime=dt, duration=duration)
+                reviews.append(review)
+        return reviews
